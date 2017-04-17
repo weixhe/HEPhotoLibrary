0c0c17d70c781ec6797301f4bb9ede973ab5a731
weixhe <workerwei@163.com>
2017-04-17 22:27:06 +0800

    commit Project

---
+++ HEPhotoLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HEPhotoLibrary
+//
+//  Created by weixhe on 2017/4/17.
+//  Copyright © 2017年 weixhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HEPhotoLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HEPhotoLibrary
+//
+//  Created by weixhe on 2017/4/17.
+//  Copyright © 2017年 weixhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
