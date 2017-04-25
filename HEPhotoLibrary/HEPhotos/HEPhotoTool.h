9e2a190e1a8d56348faf2267d17ff1ade40a8efa
weixhe <workerwei@163.com>
2017-04-25 22:35:03 +0800

    将Asset转化成图片

--- HEPhotoLibrary/HEPhotos/HEPhotoTool.h
+++ HEPhotoLibrary/HEPhotos/HEPhotoTool.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import <Photos/Photos.h>
 
 @interface HEPhotoAblumList : NSObject
--- HEPhotoLibrary/HEPhotos/HEPhotoTool.m
+++ HEPhotoLibrary/HEPhotos/HEPhotoTool.m
@@ -8,6 +8,10 @@
 
 #import "HEPhotoTool.h"
 
+#define kViewWidth      [[UIScreen mainScreen] bounds].size.width
+#define kViewHeight     [[UIScreen mainScreen] bounds].size.height
+
+
 @implementation HEPhotoAblumList
 
 
@@ -124,10 +128,59 @@ static HEPhotoTool *instance = nil;
 }
 
 
-//#pragma mark - 获取asset对应的图片
-//
-//- (void)requestImage
+#pragma mark - 获取asset对应的图片
 
+/*!
+ *  @brief resizeMode：对请求的图像怎样缩放::None-默认加载方式；  Fast-尽快地提供接近或稍微大于要求的尺寸； Exact-精准提供要求的尺寸。
+ */
+
+- (void)requestImageForAsset:(PHAsset *)asset size:(CGSize)size resizeMode:(PHImageRequestOptionsResizeMode)resizeMode
+                    complete:(void(^)(UIImage *, NSDictionary *))complete {
+    // 请求大图界面，当切换图片时，取消上一张图片的请求，对于iCloud端的图片，可以节省流量
+    static PHImageRequestID requestID = -1;
+    CGFloat scale = [UIScreen mainScreen].scale;        // @1x, @2x, @3x
+    CGFloat width = MIN(kViewWidth, kViewHeight);
+    
+    if (requestID >= 1 && size.width / width == scale) {
+        // 取消请求
+        [[PHCachingImageManager defaultManager] cancelImageRequest:requestID];
+    }
+    
+    PHImageRequestOptions *option = [[PHImageRequestOptions alloc] init];
+
+    /**
+     resizeMode：对请求的图像怎样缩放。有三种选择：
+     None，默认加载方式；
+     Fast，尽快地提供接近或稍微大于要求的尺寸；
+     Exact，精准提供要求的尺寸。
+     
+     deliveryMode：图像质量。有三种值：
+     Opportunistic，在速度与质量中均衡；
+     HighQualityFormat，不管花费多长时间，提供高质量图像；
+     FastFormat，以最快速度提供好的质量。
+     
+     这个属性只有在 synchronous 为 true 时有效。
+     */
+    option.resizeMode = resizeMode;
+    option.networkAccessAllowed = YES;   // YES-接受从iCloud下载，NO-不接受iCloud
+    option.synchronous = YES;
+    
+    /*
+     info字典提供请求状态信息:
+     PHImageResultIsInCloudKey：图像是否必须从iCloud请求
+     PHImageResultIsDegradedKey：当前UIImage是否是低质量的，这个可以实现给用户先显示一个预览图
+     PHImageResultRequestIDKey和PHImageCancelledKey：请求ID以及请求是否已经被取消
+     PHImageErrorKey：如果没有图像，字典内的错误信息
+     */
+    requestID = [[PHCachingImageManager defaultManager] requestImageForAsset:asset targetSize:size contentMode:PHImageContentModeAspectFit options:option resultHandler:^(UIImage * _Nullable image, NSDictionary * _Nullable info) {
+        BOOL downloadFinined = ![[info objectForKey:PHImageCancelledKey] boolValue] && ![info objectForKey:PHImageErrorKey];
+        //不要该判断，即如果该图片在iCloud上时候，会先显示一张模糊的预览图，待加载完毕后会显示高清图
+        // && ![[info objectForKey:PHImageResultIsDegradedKey] boolValue]
+        if (downloadFinined && complete) {
+            complete(image, info);
+        }
+    }];
+}
 
 
 
