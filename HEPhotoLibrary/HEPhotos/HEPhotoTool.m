7bd282ca2170e492b1814b828d02af3554187367
weixhe <workerwei@163.com>
2017-04-25 21:44:35 +0800

    添加获取所有的相册列表的方法

--- HEPhotoLibrary/HEPhotos/HEPhotoTool.m
+++ HEPhotoLibrary/HEPhotos/HEPhotoTool.m
@@ -8,6 +8,16 @@
 
 #import "HEPhotoTool.h"
 
+@implementation HEPhotoAblumList
+
+
+@end
+
+
+
+
+#pragma mark -|
+
 static HEPhotoTool *instance = nil;
 @implementation HEPhotoTool
 
@@ -22,24 +32,37 @@ static HEPhotoTool *instance = nil;
 #pragma mark - 获取所有相册列表
 
 /*!
- *  @brief 获取用户所有相册列表
+ *  @brief 获取用户所有相册列表，并遍历相册，获取每个相册的相关信息（相册名，照片个数，第一种缩略图等）
  */
-//- (NSArray <HEPhotoAblumList *> *)getPhotoAblumList {
-//    
-//    NSMutableArray *array = [NSMutableArray array];
-//    
-//    // 获取所有的智能相册
-//    PHFetchResult *result = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-//    
-//    [result enumerateObjectsUsingBlock:^(PHAssetCollection * _Nonnull collection, NSUInteger idx, BOOL * _Nonnull stop) {
-//        // 过滤掉视频和已删除
-//        if (collection.assetCollectionSubtype != 202 || collection.assetCollectionSubtype < 212) {
-//            <#statements#>
-//        }
-//        
-//    }];
-//    
-//}
+- (NSArray <HEPhotoAblumList *> *)getPhotoAblumList {
+    
+    NSMutableArray *array = [NSMutableArray array];
+    
+    // 获取所有的智能相册
+    PHFetchResult *result = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    
+    // 遍历每个相册, 从相册中取asset
+    [result enumerateObjectsUsingBlock:^(PHAssetCollection * _Nonnull collection, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        // 过滤掉视频和已删除
+        if (collection.assetCollectionSubtype != 202 || collection.assetCollectionSubtype < 212) {
+            
+            // 从相册中取资源
+            NSArray <PHAsset *> *assets = [self getAssetsInAssetCollection:collection ascending:NO];
+            if (assets.count != 0) {
+                HEPhotoAblumList *ablum = [[HEPhotoAblumList alloc] init];
+                ablum.title = collection.localizedTitle;            // 相册名字
+                ablum.count = assets.count;                         // 该相册内相片数量
+                ablum.headImageAsset = assets.firstObject;          // 相册第一张图片缩略图
+                ablum.assetCollection = collection;                 // 相册集，通过该属性获取该相册集下所有照片
+                
+                [array addObject:ablum];
+            }
+        }
+        
+    }];
+    return array;
+}
 
 /*!
  *  @brief 获取指定相册内的所有图片
@@ -63,6 +86,7 @@ static HEPhotoTool *instance = nil;
     
     PHFetchOptions *option = [[PHFetchOptions alloc] init];
     option.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:ascending]];
+    
     PHFetchResult *result = [PHAsset fetchAssetsInAssetCollection:collection options:option];
     return result;
 }
--- HEPhotoLibrary/ViewController.m
+++ HEPhotoLibrary/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "HEPhotoTool.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [[HEPhotoTool sharePhotoTool] getPhotoAblumList];
+    });
 }
 
 
